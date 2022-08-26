bacceef27297e971a671b4c20a46fca1b334a6e9
ongyjho <ongyjho@gmail.com>
2022-08-26 17:43:20 +0900

    .

--- src/jsMain/kotlin/todoapp/TodoClientApplication.kt
+++ src/jsMain/kotlin/todoapp/TodoClientApplication.kt
@@ -1,6 +1,9 @@
 package todoapp
 
 import kotlinx.browser.document
+import kotlinx.html.div
+import kotlinx.html.dom.create
+import kotlinx.html.h1
 
 /**
  * 클라이언트 애플리케이션 진입점(entry point)
@@ -10,5 +13,11 @@ import kotlinx.browser.document
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
 
-    TODO("Client application entry point!")
+    container.appendChild(
+        document.create.div {
+            h1 {
+                +"TODO App"
+            }
+        }
+    )
 }
